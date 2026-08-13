ad6a8e94410e765fc5fb735f92072c8fd0cbc788
Kushagra <kushagra2080@gmail.com>
2026-08-13 11:17:22 +0530

    Add files via upload

---
+++ uvm_verification/spi_sequencer.sv
@@ -0,0 +1,17 @@
+`ifndef SPI_SEQUENCER_SV
+`define SPI_SEQUENCER_SV
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "spi_seq_item.sv"
+
+class spi_sequencer extends uvm_sequencer #(spi_seq_item);
+    `uvm_component_utils(spi_sequencer)
+
+    function new(string name = "spi_sequencer", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+endclass
+
+`endif
